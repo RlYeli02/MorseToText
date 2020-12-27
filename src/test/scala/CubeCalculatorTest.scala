578c9c20bd5a7622b61f452f7d8bf63fca5c50aa
Franyeliza <franyelizarodriguez@live.com>
2020-12-27 19:39:07 -0400

    SetUp

---
+++ src/test/scala/CubeCalculatorTest.scala
@@ -0,0 +1,9 @@
+class MorseToTextTest extends org.scalatest.funsuite.AnyFunSuite {
+  test(".--- ..- .-. .- ... .. -.-. --- .-.-. debe ser 'jurasico'"){
+    var expected = "jurasico";
+    var actual = ".--- ..- .-. .- ... .. -.-. --- .-.-."
+    assert(Morse.Morse.MorseToTextTest(actual)== expected)
+  }
+
+  
+}
